6d59c75376e4893a9ce7a37719e6fc81834a307a
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2025-02-17 14:19:17 +0900

    Starting point for Lexer
    Fighting with mutable variables and borrowed values

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,22 +2,47 @@ pub mod token;
 mod token_type;
 
 use crate::scanner::token::Token;
+use crate::scanner::token_type::TokenType;
 
 pub struct Scanner {
     source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
 }
 
 impl Scanner {
     pub fn new(source: String) -> Scanner {
-        Scanner { source }
+        Scanner {
+            source,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+        }
     }
 
-    pub fn print_source(&self) {
-        println!("{}", self.source);
+    fn is_at_end(&self) -> bool {
+        return self.current >= self.source.chars().count();
     }
 
-    pub fn scan_tokens(&self) -> Vec<Token> {
-        Vec::new()
+    fn scan_token(&self) {
+        // TODO: implement it
+    }
+
+    pub fn scan_tokens(&mut self) -> &Vec<Token> {
+        while !self.is_at_end() {
+            self.scan_token()
+        }
+        self.tokens.push(Token::new(
+            TokenType::Eof,
+            String::from(""),
+            String::from(""),
+            42,
+        ));
+
+        &self.tokens
     }
 }
 
@@ -28,11 +53,13 @@ mod tests {
 
     #[test]
     fn scan_empty() {
-        let scanner = Scanner::new(String::from(""));
+        let mut scanner = Scanner::new(String::from(""));
         let tokens = scanner.scan_tokens();
 
-        //Basic check if we get vectors of Tokens
+        // Basic check if we get vectors of Tokens
         assert!(type_name_of_val(&tokens).contains("vec::Vec<"));
         assert!(type_name_of_val(&tokens).contains("token::Token>"));
+        // Should be end of file token
+        assert_eq!(tokens.len(), 1);
     }
 }
